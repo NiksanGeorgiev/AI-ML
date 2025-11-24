dc2f66c533250bb22dee6f61dca6a74129e3691c
NiksanGeorgiev <nikolaypavgr@gmail.com>
2025-11-24 11:27:20 +0100

    finished assignment 2

--- Week2/assignment_2.pl
+++ Week2/assignment_2.pl
@@ -42,3 +42,49 @@ stay_home(Person) :- person(Person), corona_symptom(Symptom), has_symptom(Person
 stay_home(Person) :- person(Person), housemate(Housemate, Person), has_corona(Housemate).
 
 stay_home(Person) :- person(Person), housemate(Person, Housemate), has_corona(Housemate).
+
+% Part II
+person(mary).
+person(john).
+person(tabitha).
+
+public_building(store).
+public_building(museum).
+public_building(municipality).
+public_building(station).
+
+activity(working).
+activity(commuting).
+activity(shopping).
+activity(cycling).
+activity(theatre).
+activity(dance).
+
+visit(store, shopping).
+visit(station, commuting).
+
+contact_profession(physiotherapy).
+contact_profession(pedicure).
+
+profession(mary, physiotherapy).
+profession(john, pedicure).
+profession(tabitha, scientist).
+
+personal_mask_exception(john).
+activity_mask_exception(theatre).
+activity_mask_exception(dance).
+
+% 3. Wear a mask for activity should be true for any activity but the ones with exception
+% NOTE: We did look up online how to perform an negation in Prolog because otherwise we felt that we would need ot input all activities and people that don't require a mask manually
+wear_mask(Activity) :- activity(Activity), \+ activity_mask_exception(Activity).
+wear_mask(Activity, Person) :- wear_mask(Activity), person(Person), visit(Building, Activity), public_building(Building), \+ personal_mask_exception(Person).
+
+% 4. Check if a person's profession is a contact profession to see if they need to wear a mask.
+wear_mask(working, Person) :- person(Person), profession(Person, Profession), contact_profession(Profession), \+ personal_mask_exception(Person).
+
+% 5. Exceptions for not wearing a mask using an OR condition
+no_mask(Activity, Person) :- activity(Activity), person(Person), activity_mask_exception(Activity).
+no_mask(Activity, Person) :- activity(Activity), person(Person), personal_mask_exception(Person).
+
+% 6. If we want to address the specific conflict mentioned in the exercise description, we can add the personal_mask_exception to the wear_mask rule for activities if we know for a fact personal_mask_exception is valid for any activity. 
+% If we want to address all possible conflicts, then we would want fully work out all possible cases for wear_mask for example. Given the nature of the two statements, it is expected that one is equal to the negation of the other, so by ensuring wear_mask covers all cases, we can simply use negation to determine no_mask.
